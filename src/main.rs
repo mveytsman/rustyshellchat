631e06d5163912437e875da34695de47cde0c47a
Max Veytsman <mveytsman@github.com>
2019-09-27 01:50:25 -0400

    Basic chat

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,61 @@ use std::sync::mpsc::channel;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 
-struct Clients {
-    clients: HashMap<u16, Sender<ServerMessage>>,
-}
+type ClientId = u16;
 
 enum ClientMessage {
-    Joined(u16, Sender<ServerMessage>),
-    Left(u16),
+    Joined(ClientId, Sender<ServerMessage>),
+    Left(ClientId),
+    Message(ClientId, String),
 }
 
-type ServerMessage = ClientMessage; // for now
+enum ServerMessage {
+    Joined(ClientId),
+    Left(ClientId),
+    Message(ClientId, String),
+}
 
-impl Clients {
-    fn new() -> Clients {
-        Clients {
-            clients: HashMap::new(),
+fn run_server() -> Sender<ClientMessage> {
+    let (sender, receiver) = channel();
+    thread::spawn(move || {
+        let mut clients: HashMap<ClientId, Sender<ServerMessage>> = HashMap::new();
+        for message in receiver {
+            match message {
+                ClientMessage::Joined(id, sender) => {
+                    for sender in clients.values() {
+                        sender.send(ServerMessage::Joined(id)).unwrap();
+                    }
+                    clients.insert(id, sender);
+                    println!("Joined by {}", id);
+                }
+                ClientMessage::Left(id) => {
+                    clients.remove(&id);
+                    for sender in clients.values() {
+                        sender.send(ServerMessage::Left(id)).unwrap();
+                    }
+                    println!("{} left", id);
+                }
+                ClientMessage::Message(sender_id, body) => {
+                    for (id, sender) in clients.iter() {
+                        println!("loop");
+                        if *id != sender_id {
+                            // don't send to the sender
+                            sender
+                                .send(ServerMessage::Message(sender_id, body.clone()))
+                                .unwrap();
+                        }
+                    }
+                    println!("{}: {}", sender_id, body)
+                }
+            }
         }
-    }
-    fn add(&mut self, id: u16, channel: Sender<ServerMessage>) {
-        self.clients.insert(id, channel);
-    }
-    fn remove(&mut self, id: &u16) {
-        self.clients.remove(id).unwrap();
-    }
+    });
+    sender
 }
 
 fn main() {
-    let (server_tx, server_rx) = channel();
-    thread::spawn(move || server(server_rx));
     let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+    let server_tx = run_server();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         let server_tx = Sender::clone(&server_tx);
@@ -43,25 +69,21 @@ fn main() {
     }
 }
 
-fn server(receiver: Receiver<ClientMessage>) {
-    let mut clients = Clients::new();
-    for message in receiver {
-        match message {
-            ClientMessage::Joined(id, sender) => {
-                clients.add(id, sender);
-                println!("Joined by {}", id);
-            }
-            ClientMessage::Left(id) => {
-                clients.remove(&id);
-                println!("{} left", id),
-            }
-        }
-    }
-}
-
 fn handle_client(stream: TcpStream, server_sender: Sender<ClientMessage>) {
     let client_id = stream.peer_addr().unwrap().port();
     let (client_sender, client_receiver) = channel();
+    let mut stream2 = stream.try_clone().unwrap();
+    thread::spawn(move || {
+        for message in client_receiver {
+            match message {
+                ServerMessage::Joined(id) => writeln!(&mut stream2, "Joined {}", id).unwrap(),
+
+                ServerMessage::Left(id) => writeln!(&mut stream2, "{} left", id).unwrap(),
+
+                ServerMessage::Message(id, body) => writeln!(stream2, "{}: {}", id, body).unwrap(),
+            }
+        }
+    });
     server_sender
         .send(ClientMessage::Joined(
             client_id,
@@ -69,8 +91,10 @@ fn handle_client(stream: TcpStream, server_sender: Sender<ClientMessage>) {
         ))
         .unwrap();
 
-    for l in BufReader::new(stream).lines() {
-        println!("{}", l.unwrap());
+    for line in BufReader::new(stream).lines() {
+        server_sender
+            .send(ClientMessage::Message(client_id, line.unwrap()))
+            .unwrap();
     }
     server_sender.send(ClientMessage::Left(client_id)).unwrap();
 }
